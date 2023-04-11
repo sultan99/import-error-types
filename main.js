275f6647e1b75984cea829b708a9a69a242e6c28
sultan99 <sultan99@mail.ru>
2023-04-12 00:41:27 +0200

    fix with type-error exception

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-const load = url => import(url).catch(error => ({isNetworkError: error instanceof TypeError}))
+const load = url => import(url).catch(
+  error => error instanceof TypeError
+    ? fetch(url).then(res => ({isNetworkError: res.status === 404}))
+    : Promise.resolve({isNetworkError: false})
+)
 
 
 const start = async () => {
   const correct = await load('./correct.js')
   const hasException = await load('./has-exception.js')
-  const wrongUrl = await load('./wrong-url.js')
+  const hasNetworkLikeException = await load('./has-network-like-exception.js')
+  const hasNetworkException = await load('./wrong-url.js')
   
-  console.log({correct, hasException, wrongUrl})
+  console.log({
+    correct,
+    hasException,
+    hasNetworkLikeException,
+    hasNetworkException,
+  })
 }
 
 start()
